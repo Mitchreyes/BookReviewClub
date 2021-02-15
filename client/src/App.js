e11332a384597a51e797bc23565a22486c0b616a
Mitchreyes <mitchreyes@gmail.com>
2021-02-14 18:56:16 -0800

    Added React-Redux, Token authentication useEffect on App.js, and setup auth and alert actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../src/components/home/Home';
+import Navbar from '../src/components/layout/Navbar';
+import Routes from './components/routing/Routes.js';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import { LOGOUT } from './actions/types';
+import setAuthToken from './utils/setAuthToken';
+
 import './style/main.scss';
 
-const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
-);
+const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
