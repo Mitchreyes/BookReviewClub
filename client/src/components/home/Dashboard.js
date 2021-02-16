3c4c9f0582e7b5c7c23521995e6f7a0508b0dea6
Mitchreyes <mitchreyes@gmail.com>
2021-02-16 15:35:38 -0800

    Added social sliders styling to Dashboard component

--- client/src/components/home/Dashboard.js
+++ client/src/components/home/Dashboard.js
@@ -1,28 +1,96 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import DashboardActions from './DashboardActions';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
+const initialState = {
+  location: '',
+  bio: '',
+  twitter: '',
+  facebook: '',
+  youtube: '',
+  instagram: '',
+};
+
 const Dashboard = ({
   getCurrentProfile,
   deleteAccount,
   auth: { user },
-  profile: { profile },
+  profile: { profile, loading },
 }) => {
+  const [formData, setFormData] = useState(initialState);
+
   useEffect(() => {
     getCurrentProfile();
-  }, [getCurrentProfile]);
+    if (!loading && profile) {
+      const profileData = { ...initialState };
+      for (const key in profile) {
+        if (key in profileData) profileData[key] = profile[key];
+      }
+      for (const key in profile.social) {
+        if (key in profileData) profileData[key] = profile.social[key];
+      }
+      setFormData(profileData);
+    }
+  }, [loading, profile, getCurrentProfile]);
+
+  const { location, bio, twitter, facebook, youtube, instagram } = formData;
 
   return (
     <div className='dashboard-wrapper'>
-      <h1 className='large text-primary dashboard-title'>Dashboard</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome {user && user.name}
+      {/* <h1 className='large text-primary dashboard-title'>Dashboard</h1> */}
+      <p className='lead dashboard-welcome'>
+        <i className='fas fa-user' />
+        {user && user.name}
       </p>
       {profile !== null ? (
         <div className='dashboard'>
+          <div className='dashboard-social'>
+            <ul className='socials'>
+              <li class='twitter'>
+                <a href={twitter}>
+                  <i className='fab fa-twitter fa-2x' aria-hidden='true' />
+                  <div className='slider'>
+                    <p>twitter</p>
+                  </div>
+                </a>
+              </li>
+              <li class='facebook'>
+                <a href={facebook}>
+                  <i className='fab fa-facebook fa-2x' aria-hidden='true' />
+                  <div className='slider'>
+                    <p>facebook</p>
+                  </div>
+                </a>
+              </li>
+              <li class='youtube'>
+                <a href={youtube}>
+                  <i className='fab fa-youtube fa-2x' aria-hidden='true' />
+                  <div className='slider'>
+                    <p>youtube</p>
+                  </div>
+                </a>
+              </li>
+              <li class='instagram'>
+                <a href={instagram}>
+                  <i className='fab fa-instagram fa-2x' aria-hidden='true' />
+                  <div className='slider'>
+                    <p>instagram</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='dashboard-bio-wrapper'>
+            <p className='lead'>About</p>{' '}
+            <div className='dashboard-bio'>{bio}</div>
+          </div>
+          <div className='dashboard-location-wrapper'>
+            <div className='dashboard-location'>{location}</div>
+          </div>
           <DashboardActions className='dashboard-actions' />
 
           <div className='my-2 dashboard-delete'>
